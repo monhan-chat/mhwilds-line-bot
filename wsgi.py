be92961bc524a0e9730767429ae8b726f0feffbb
monhan-chat <monhanchattobot@gmail.com>
2025-03-11 17:13:43 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
-from app import app
+# wsgi.py
+import sys
+import os
 
-if __name__ == "__main__":
-    app.run()
+# カレントディレクトリをPYTHONPATHに追加
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+# アプリケーションをインポート
+from app import app as application
+
+# Gunicornは'application'という名前の変数も探すので、両方定義しておく
+app = application
